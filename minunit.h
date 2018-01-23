bc8dfe5ce9a64d193c847ac45c51406d15e2076b
Kacper Kowalski <kacper.kowalski@novamedia.pl>
2018-01-23 20:23:19 +0100

    Add function which compares string at n chars

--- minunit.h
+++ minunit.h
@@ -251,6 +251,24 @@ static void (*minunit_teardown)(void) = NULL;
 	}\
 )
 
+#define mu_assert_string_n_eq(expected, result, complen) MU__SAFE_BLOCK(\
+	const char* minunit_tmp_e = expected;\
+	const char* minunit_tmp_r = result;\
+	minunit_assert++;\
+	if (!minunit_tmp_e) {\
+		minunit_tmp_e = "<null pointer>";\
+	}\
+	if (!minunit_tmp_r) {\
+		minunit_tmp_r = "<null pointer>";\
+	}\
+	if(strncmp(minunit_tmp_e, minunit_tmp_r, complen)) {\
+		snprintf(minunit_last_message, MINUNIT_MESSAGE_LEN, "%s failed:\r\n\t%s:%d: '%s' expected but was '%s'", __func__, __FILE__, __LINE__, minunit_tmp_e, minunit_tmp_r);\
+		minunit_status = 1;\
+		return;\
+	} else {\
+		printf(".");\
+	}\
+)
 #define mu_assert_bytearray_eq(expected, result, complen) MU__SAFE_BLOCK(\
 	const uint8_t* minunit_tmp_e = expected;\
 	const uint8_t* minunit_tmp_r = result;\
